75ad31e4d55fa47b287108ec262c7b0602e217ed
wiggins-j <jonisavg@gmail.com>
2026-07-12 13:50:09 -0400

    errorta 0.1.0-alpha.0 (darwin/arm64)

--- Formula/errorta.rb
+++ Formula/errorta.rb
@@ -8,7 +8,7 @@ class Errorta < Formula
   on_macos do
     on_arm do
       url "https://github.com/wiggins-j/errorta_app/releases/download/cli-v0.1.0-alpha.0/errorta-0.1.0-alpha.0-darwin-arm64.tar.gz"
-      sha256 "f6d9699ca918f6634e5a8bddaa2a0ac43da42dcd5aabd86e149fe620c3f65b93"
+      sha256 "caede526ffc698b01d76888fbb6355cc6efdd03b304c0d49ca612b8d23858780"
     end
   end
 
