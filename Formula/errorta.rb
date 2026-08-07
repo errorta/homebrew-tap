0781ab6235262bd484377661c89c3f8273293dce
wiggins-j <jonisavg@gmail.com>
2026-08-07 12:56:03 -0400

    errorta 0.1.0-alpha.17 (darwin/arm64)

--- Formula/errorta.rb
+++ Formula/errorta.rb
@@ -1,14 +1,14 @@
 class Errorta < Formula
   desc "Headless AI coding-council CLI"
   homepage "https://github.com/wiggins-j/errorta_app"
-  version "0.1.0-alpha.16"
+  version "0.1.0-alpha.17"
   license "Apache-2.0"
   depends_on arch: :arm64
 
   on_macos do
     on_arm do
-      url "https://github.com/wiggins-j/errorta_app/releases/download/cli-v0.1.0-alpha.16/errorta-0.1.0-alpha.16-darwin-arm64.tar.gz"
-      sha256 "5cd54bff7a5cb6a2ddac95fd247ca34deb8d0724ca6280bf5042d072576a4d2b"
+      url "https://github.com/wiggins-j/errorta_app/releases/download/cli-v0.1.0-alpha.17/errorta-0.1.0-alpha.17-darwin-arm64.tar.gz"
+      sha256 "ec804c35e4a4327c8404da87d1ebf64beb1489509a382c86f799fd2bf4bc9a6a"
     end
   end
 
